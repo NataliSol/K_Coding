05c68221d7682aeaff20e2029e2ccc20c9dffeea
nat_soloviova <natsolovyova2@gmail.com>
2025-11-29 20:01:54 +0100

    add Usermanager.class, pom.xml

---
+++ K_Coding/src/main/java/UserManager.java
@@ -0,0 +1,49 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManager {
+    public List<String> allUsers = new ArrayList<>();
+    private static final Logger LOGGER = LogManager.getLogger(UserManager.class);
+
+    public void createUser(String username) {
+        LOGGER.info("процес створення юзера запущений");
+        if (username != null) {
+            allUsers.add(username);
+        } else
+            LOGGER.warn("додавання юзера пройшло не успішно");
+    }
+
+    public void deleteUser(String username) {
+        if (username != null) {
+            allUsers.remove(username);
+        } else
+            LOGGER.warn("видалення юзера пройшло не успішно");
+    }
+
+    public void findUser(String username) {
+        boolean found = false;
+        for (String user : allUsers) {
+            if (user.equals(username)) {
+                System.out.println("Юзер знайдений");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            LOGGER.warn("Юзер не знайдений");
+        }
+    }
+
+    public void userUpdate(String username) {
+        LOGGER.info("процес створення юзера запущений");
+        try {
+            findUser(username);
+            String cryptoName = username + "12233ew";
+        } catch (NullPointerException e) {
+            LOGGER.error("юзер не знайдений");
+        }
+    }
+}
