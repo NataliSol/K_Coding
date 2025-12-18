d9ed325ac1329c437a6798d68d26e12b72d2a524
nat_soloviova <natsolovyova2@gmail.com>
2025-12-18 12:52:49 +0100

    add flowManagementTask

---
+++ K_Coding/src/main/java/flowManagementTask/MyThread.java
@@ -0,0 +1,16 @@
+package flowManagementTask;
+
+public class MyThread implements Runnable {
+    String name;
+
+    public MyThread(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("потік MyThread працює");
+        }
+    }
+}
